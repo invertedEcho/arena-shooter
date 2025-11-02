72817d5b65a88bd5810f3ac2b5e4b13e15031ade
Jakob Stechow <jakob.stechow@pm.me>
2025-11-02 13:21:34 +0100

    wip: sub states

--- src/game_flow/mod.rs
+++ src/game_flow/mod.rs
@@ -5,14 +5,15 @@ use crate::{
         game_mode::GameModePlugin,
         score::GameScorePlugin,
         states::{
-            AppDebugState, AppState, GameLoadingState, InGameState,
+            AppDebugState, AppState, InGameState, LoadingGameSubState,
             MainMenuState, SelectedMapState,
         },
         systems::{
             check_collider_constructor_hierarchy_ready, check_navmesh_ready,
-            check_world_scene_loaded, free_mouse, grab_mouse, handle_escape,
-            handle_exit_in_game, handle_playing_state_enter,
-            on_game_loading_state_nav_mesh_ready, spawn_main_menu_camera,
+            check_world_scene_loaded, free_mouse, grab_mouse,
+            handle_escape_in_game, handle_exit_in_game,
+            handle_playing_state_enter, on_game_loading_state_nav_mesh_ready,
+            spawn_main_menu_camera,
         },
     },
     player::PlayerDeathMessage,
@@ -29,18 +30,18 @@ pub struct GameFlowPlugin;
 impl Plugin for GameFlowPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<AppState>()
-            .init_state::<InGameState>()
-            .init_state::<MainMenuState>()
             .init_state::<AppDebugState>()
-            .init_state::<GameLoadingState>()
             .init_state::<SelectedMapState>()
+            .add_sub_state::<InGameState>()
+            .add_sub_state::<MainMenuState>()
+            .add_sub_state::<LoadingGameSubState>()
             .add_message::<PlayerDeathMessage>()
             .add_plugins(GameScorePlugin)
             .add_plugins(GameModePlugin)
             .add_observer(check_collider_constructor_hierarchy_ready)
             .add_observer(check_navmesh_ready)
             .add_systems(
-                OnEnter(GameLoadingState::NavMeshReady),
+                OnEnter(LoadingGameSubState::NavMeshReady),
                 on_game_loading_state_nav_mesh_ready,
             )
             .add_systems(
@@ -50,7 +51,10 @@ impl Plugin for GameFlowPlugin {
             .add_systems(OnEnter(InGameState::Paused), free_mouse)
             .add_systems(OnExit(AppState::InGame), handle_exit_in_game)
             .add_systems(Startup, spawn_main_menu_camera)
-            .add_systems(Update, (check_world_scene_loaded, handle_escape))
+            .add_systems(
+                Update,
+                (check_world_scene_loaded, handle_escape_in_game),
+            )
             .add_systems(
                 Update,
                 check_world_scene_loaded
