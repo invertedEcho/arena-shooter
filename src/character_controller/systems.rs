348d0dfc7e767a6ae2718c448187cb765b8c1ca9
Jakob Stechow <jakob.stechow@pm.me>
2025-12-24 13:38:25 +0100

    wip

--- src/character_controller/systems.rs
+++ src/character_controller/systems.rs
@@ -82,13 +82,14 @@ pub fn handle_keyboard_input_for_player(
     });
 }
 
+const MAX_DISTANCE_SHAPE_CAST_MOVEMENT: f32 = 0.3;
 pub fn handle_movement_actions_for_character_controllers(
     mut movement_action_reader: MessageReader<MovementAction>,
     mut character_controller_query: Query<
         (&mut LinearVelocity, &Grounded, &Transform),
         With<CharacterController>,
     >,
-    spatial_query: SpatialQuery,
+    mut spatial_query: SpatialQuery,
     time: Res<Time>,
     medkit_query: Query<Entity, With<Medkit>>,
 ) {
@@ -106,108 +107,147 @@ pub fn handle_movement_actions_for_character_controllers(
             continue;
         };
 
+        // TODO: jump and move should really be the same, we just have a function that probably
+        // needs to be recursive and checks with shapecast and rejects
         match *direction {
             MovementDirection::Jump => {
                 if grounded.0 {
                     velocity.y = JUMP_VELOCITY;
                 }
             }
-            // TODO: should probably move the content of this block elsewhere
             MovementDirection::Move(world_velocity) => {
-                let Ok(direction_from_world_velocity) =
-                    Dir3::new(world_velocity)
-                else {
-                    velocity.x = 0.0;
-                    velocity.z = 0.0;
-                    return;
-                };
-
-                let ray_origin = transform.translation
-                    - direction_from_world_velocity.as_vec3() * 0.025;
-                let max_distance = 0.3;
-
                 let excluded_entities: Vec<Entity> = medkit_query
                     .iter()
                     .chain(std::iter::once(character_controller_entity))
                     .collect();
-
-                // also exclude medkits
-                let spatial_query_filter = &SpatialQueryFilter::default()
-                    .with_excluded_entities(excluded_entities);
-
-                if let Some(hit_ahead) = spatial_query.cast_shape(
-                    &Collider::capsule(
-                        CHARACTER_CAPSULE_RADIUS,
-                        CHARACTER_CAPSULE_LENGTH,
-                    ),
-                    ray_origin,
-                    transform.rotation,
-                    direction_from_world_velocity,
-                    &ShapeCastConfig {
-                        max_distance,
-                        ..default()
-                    },
-                    spatial_query_filter,
-                ) {
-                    // obstacle in the way, check if we can slimb it
-                    // a normal is just a direction something is facing
-                    let normal = hit_ahead.normal1;
-                    let slope_angle = normal.angle_between(Vec3::Y);
-                    let slope_climable = slope_angle < MAX_SLOPE_ANGLE;
-
-                    if slope_climable {
-                        debug!("MOVEMENT: Climable slope!");
-                        // this is the most important part to make the slope climbing possible.
-                        // instead of trying to go straight, we slide along the ground
-                        velocity.0 =
-                            world_velocity.reject_from_normalized(normal);
-
-                        // slope snapping
-                        let ray_down_origin =
-                            transform.translation + Vec3::Y * 0.5;
-                        let ray_down_direction = Dir3::NEG_Y;
-                        let max_down_distance = 1.0;
-
-                        if let Some(hit_down) = spatial_query.cast_ray(
-                            ray_down_origin,
-                            ray_down_direction,
-                            max_down_distance,
-                            true,
-                            spatial_query_filter,
-                        ) {
-                            let hit_down_point = ray_down_origin
-                                + ray_down_direction * hit_down.distance;
-                            let hit_down_y = hit_down_point.y;
-                            let player_y = transform.translation.y;
-                            let difference_y = hit_down_y - player_y;
-                            if difference_y.abs() < 0.3 {
-                                debug!(
-                                    "Snapping character controller to slope"
-                                );
-                                velocity.y = difference_y / time.delta_secs();
-                            }
-                        }
-                    } else {
-                        debug!(
-                            "MOVEMENT: Obstacle in the way, sliding along wall"
-                        );
-                        // not climable, e.g. a wall. we want to slide along the wall, similar to the collide
-                        // and slide algorithm
-                        // the main difference is that we ignore the Y part, because its too step, so we dont
-                        // want to climb up
-                        let impulse =
-                            world_velocity.reject_from_normalized(normal);
-                        velocity.x = impulse.x;
-                        velocity.z = impulse.z
-                    }
-                } else {
-                    debug!("MOVEMENT: No obstacle ahead, free movement");
-                    // no obstacle ahead, free movement
-                    velocity.x = world_velocity.x;
-                    velocity.z = world_velocity.z;
-                }
+                apply_collide_and_slide(
+                    &mut velocity,
+                    world_velocity,
+                    &excluded_entities,
+                    transform,
+                    &mut spatial_query,
+                    time.delta_secs(),
+                    0,
+                );
             }
         }
+    }
+}
+
+fn apply_collide_and_slide(
+    current_velocity: &mut Vec3,
+    desired_velocity: Vec3,
+    excluded_entities: &Vec<Entity>,
+    origin_transform: &Transform,
+    spatial_query: &mut SpatialQuery,
+    time_delta_secs: f32,
+    current_hit_count: usize,
+) {
+    const MAX_HITS: usize = 5;
+    let Ok(direction_from_world_velocity) = Dir3::new(desired_velocity) else {
+        return;
+    };
+
+    if desired_velocity.length_squared() < 0.0001 {
+        *current_velocity = Vec3::splat(0.0);
+        return;
+    }
+
+    if current_hit_count > MAX_HITS {
+        *current_velocity = Vec3::splat(0.);
+        return;
+    }
+
+    let ray_origin = origin_transform.translation
+        - direction_from_world_velocity.as_vec3() * 0.025;
+
+    // also exclude medkits
+    let spatial_query_filter = &SpatialQueryFilter::default()
+        .with_excluded_entities(excluded_entities.clone());
+
+    if let Some(hit_ahead) = spatial_query.cast_shape(
+        &Collider::capsule(CHARACTER_CAPSULE_RADIUS, CHARACTER_CAPSULE_LENGTH),
+        ray_origin,
+        origin_transform.rotation,
+        direction_from_world_velocity,
+        &ShapeCastConfig {
+            max_distance: MAX_DISTANCE_SHAPE_CAST_MOVEMENT,
+            ..default()
+        },
+        spatial_query_filter,
+    ) {
+        // obstacle in the way, check if we can slimb it
+        // a normal is just a direction something is facing
+        let normal = hit_ahead.normal1;
+        let slope_angle = normal.angle_between(Vec3::Y);
+        let slope_climable = slope_angle < MAX_SLOPE_ANGLE;
+
+        if slope_climable {
+            debug!("MOVEMENT: Climable slope!");
+            // this is the most important part to make the slope climbing possible.
+            // instead of trying to go straight, we slide along the ground
+            *current_velocity = desired_velocity.reject_from_normalized(normal);
+
+            // slope snapping
+            let ray_down_origin = origin_transform.translation + Vec3::Y * 0.5;
+            let ray_down_direction = Dir3::NEG_Y;
+            let max_down_distance = 1.0;
+
+            if let Some(hit_down) = spatial_query.cast_ray(
+                ray_down_origin,
+                ray_down_direction,
+                max_down_distance,
+                true,
+                spatial_query_filter,
+            ) {
+                let hit_down_point =
+                    ray_down_origin + ray_down_direction * hit_down.distance;
+                let hit_down_y = hit_down_point.y;
+                let player_y = origin_transform.translation.y;
+                let difference_y = hit_down_y - player_y;
+                if difference_y.abs() < 0.3 {
+                    debug!("Snapping character controller to slope");
+                    current_velocity.y = difference_y / time_delta_secs;
+                }
+            }
+        } else {
+            debug!("MOVEMENT: Obstacle in the way, sliding along wall");
+            // not climable, e.g. a wall. we want to slide along the wall,
+            // similar to the collide and slide algorithm
+            // the main difference is that we ignore the Y part,
+            // because its too step, so we dont want to climb up
+            let impulse = desired_velocity.reject_from_normalized(normal);
+            // we need to check again if the new velocity (impulse) would also penetrate an
+            // obstacle until we dont or we reach MAX_HITS, where we just zero out velocity
+            info!(
+                "obstacle in the way, sliding along wall by recursive call to \
+                 apply_collide_and_slide: {}",
+                impulse
+            );
+            let new_transform = Transform {
+                translation: origin_transform.translation
+                    + desired_velocity * time_delta_secs,
+                rotation: origin_transform.rotation,
+                scale: origin_transform.scale,
+            };
+
+            apply_collide_and_slide(
+                current_velocity,
+                impulse,
+                excluded_entities,
+                // so this makes no sense, we wont be at the same transform once we have even one
+                // recursive iteration
+                &new_transform,
+                spatial_query,
+                time_delta_secs,
+                current_hit_count + 1,
+            );
+        }
+    } else {
+        debug!("MOVEMENT: No obstacle ahead, free movement");
+        // no obstacle ahead, free movement
+        current_velocity.x = desired_velocity.x;
+        current_velocity.z = desired_velocity.z;
     }
 }
 
@@ -226,6 +266,7 @@ pub fn update_on_ground(
         }
 
         if on_ground && velocity.y <= 0.0 {
+            info!("Zeroeing out y velocity");
             velocity.y = 0.0
         }
     }
@@ -254,12 +295,15 @@ pub fn apply_movement_damping(
 
 pub fn check_above_head(
     query: Query<
-        (Entity, &mut LinearVelocity, &Transform),
+        (Entity, &mut LinearVelocity, &Transform, &Grounded),
         With<CharacterController>,
     >,
     spatial_query: SpatialQuery,
 ) {
-    for (entity_itself, mut velocity, transform) in query {
+    for (entity_itself, mut velocity, transform, grounded) in query {
+        if (grounded.0) {
+            continue;
+        };
         let Some(_) = spatial_query.cast_shape(
             &Collider::capsule(
                 CHARACTER_CAPSULE_RADIUS,
@@ -277,6 +321,7 @@ pub fn check_above_head(
 
         // if there is something above the current shape, stop vertical movement, to prevent
         // clipping into ceilings
+        info!("something above head, decreasing velocity");
         velocity.y -= 0.1;
     }
 }
