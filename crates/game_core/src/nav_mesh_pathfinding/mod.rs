46f3d3d5ffe77754dd380b79f577bc24d47d9b22
Jakob Stechow <jakob.stechow@pm.me>
2026-02-16 16:53:51 +0100

    wip

--- crates/game_core/src/nav_mesh_pathfinding/mod.rs
+++ crates/game_core/src/nav_mesh_pathfinding/mod.rs
@@ -1,15 +1,11 @@
-use avian_rerecast::AvianBackendPlugin;
-use bevy::{platform::collections::HashSet, prelude::*};
-use bevy_landmass::{debug::Landmass3dDebugPlugin, prelude::*};
-use bevy_rerecast::{Navmesh, prelude::*};
-use landmass_rerecast::{
-    Island3dBundle, LandmassRerecastPlugin, NavMeshHandle3d,
-};
+use bevy::{asset::uuid_handle, platform::collections::HashSet, prelude::*};
+use bevy_landmass::prelude::*;
 
 use shared::{
     Medkit,
     character_controller::{CHARACTER_HEIGHT, MAX_SLOPE_ANGLE},
 };
+use vleue_navigator::prelude::*;
 
 use crate::ServerLoadingState;
 
@@ -19,92 +15,113 @@ pub struct NavMeshPathfindingPlugin;
 
 impl Plugin for NavMeshPathfindingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(AvianBackendPlugin::default());
-        app.add_plugins(NavmeshPlugins::default());
-        app.add_plugins(Landmass3dPlugin::default());
-        app.add_plugins(LandmassRerecastPlugin::default());
-        app.add_plugins(Landmass3dDebugPlugin {
-            draw_on_start: false,
-            ..default()
-        });
-        app.add_systems(
-            OnEnter(ServerLoadingState::CollidersSpawned),
-            generate_navmesh_on_map_colliders_ready,
-        );
-        app.add_observer(on_navmesh_ready);
+        app.add_plugins(VleueNavigatorPlugin);
+        // app.add_plugins(AvianBackendPlugin::default());
+        // app.add_plugins(NavmeshPlugins::default());
+        // app.add_plugins(Landmass3dPlugin::default());
+        // app.add_plugins(LandmassRerecastPlugin::default());
+        // app.add_plugins(Landmass3dDebugPlugin {
+        //     draw_on_start: false,
+        //     ..default()
+        // });
+        // app.add_systems(
+        //     OnEnter(ServerLoadingState::CollidersSpawned),
+        //     generate_navmesh_on_map_colliders_ready,
+        // );
+        // app.add_observer(on_navmesh_ready);
         // app.add_systems(Update, log_agent_state);
+        app.add_systems(Startup, setup);
     }
 }
 
-// We store the NavMesh handle in a resource so we can regenerate the navmesh when needed
-#[derive(Resource)]
-pub struct NavMeshHandle(pub Handle<Navmesh>);
+#[derive(Resource, Default, Deref)]
+struct GltfHandle(Handle<Gltf>);
 
 #[derive(Resource)]
-pub struct ArchipelagoRef(pub Entity);
+pub struct CurrentNavMesh(pub Handle<NavMesh>);
 
-fn generate_navmesh_on_map_colliders_ready(
-    mut commands: Commands,
-    mut generator: NavmeshGenerator,
-    maybe_existing_nav_mesh: Option<Res<NavMeshHandle>>,
-    all_entities_except_medkits: Query<Entity, Without<Medkit>>,
-) {
-    let nav_mesh_settings = NavmeshSettings {
-        // TODO: document why this radius is smaller than ENEMY_AGENT_RADIUS
-        agent_radius: 0.3,
-        // this is pretty important, so the agent doesnt try to climb some very high ledge
-        walkable_climb: 0.25,
-        walkable_slope_angle: MAX_SLOPE_ANGLE,
-        cell_size_fraction: 2.0,
-        cell_height_fraction: 4.0,
-        agent_height: CHARACTER_HEIGHT,
-        filter: Some(HashSet::from_iter(all_entities_except_medkits)),
-        ..default()
-    };
+#[derive(Component, Clone)]
+pub struct NavMeshDisp(Handle<NavMesh>);
 
-    if let Some(existing_nav_mesh) = maybe_existing_nav_mesh {
-        info!("Nav mesh already exists, regenerating!");
-        generator.regenerate(&existing_nav_mesh.0, nav_mesh_settings);
-    } else {
-        let archipelago_options: ArchipelagoOptions<ThreeD> =
-            ArchipelagoOptions::from_agent_radius(ENEMY_AGENT_RADIUS);
+const HANDLE_TRIMESH_OPTIMIZED: Handle<NavMesh> =
+    uuid_handle!("100AD183-2C5C-49A1-AB32-142000E87828");
 
-        let archipelago_id =
-            commands.spawn(Archipelago3d::new(archipelago_options)).id();
-
-        commands.insert_resource(ArchipelagoRef(archipelago_id));
-
-        let navmesh = generator.generate(nav_mesh_settings);
-
-        commands.spawn(Island3dBundle {
-            island: Island,
-            archipelago_ref: ArchipelagoRef3d::new(archipelago_id),
-            nav_mesh: NavMeshHandle3d(navmesh),
-        });
-    }
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(GltfHandle(
+        asset_server.load("maps/main/navmesh.gltf"),
+    ));
+    commands.insert_resource(CurrentNavMesh(HANDLE_TRIMESH_OPTIMIZED));
 }
 
-fn on_navmesh_ready(
-    trigger: On<NavmeshReady>,
-    mut commands: Commands,
-    mut next_server_loading_state: ResMut<NextState<ServerLoadingState>>,
-    mut nav_meshes: ResMut<Assets<Navmesh>>,
-) {
-    let Some(nav_mesh_handle) = nav_meshes.get_strong_handle(trigger.0) else {
-        panic!(
-            "Got navmeshready event but the Handle could not be found using \
-             the asset id from the trigger"
-        );
-    };
-
-    info!("NavMesh is now ready, updating ServerLoadingState to Done");
-    next_server_loading_state.set(ServerLoadingState::Done);
-
-    commands.insert_resource(NavMeshHandle(nav_mesh_handle));
-}
-
-// fn log_agent_state(agent_state: Query<&AgentState>) {
-//     for agent_state in agent_state {
-//         info!("Agent state: {:?}", agent_state);
+// // We store the NavMesh handle in a resource so we can regenerate the navmesh when needed
+// #[derive(Resource)]
+// pub struct NavMeshHandle(pub Handle<Navmesh>);
+//
+// #[derive(Resource)]
+// pub struct ArchipelagoRef(pub Entity);
+//
+// fn generate_navmesh_on_map_colliders_ready(
+//     mut commands: Commands,
+//     mut generator: NavmeshGenerator,
+//     maybe_existing_nav_mesh: Option<Res<NavMeshHandle>>,
+//     all_entities_except_medkits: Query<Entity, Without<Medkit>>,
+// ) {
+//     let nav_mesh_settings = NavmeshSettings {
+//         // TODO: document why this radius is smaller than ENEMY_AGENT_RADIUS
+//         agent_radius: 0.3,
+//         // this is pretty important, so the agent doesnt try to climb some very high ledge
+//         walkable_climb: 0.25,
+//         walkable_slope_angle: MAX_SLOPE_ANGLE,
+//         cell_size_fraction: 2.0,
+//         cell_height_fraction: 4.0,
+//         agent_height: CHARACTER_HEIGHT,
+//         filter: Some(HashSet::from_iter(all_entities_except_medkits)),
+//         ..default()
+//     };
+//
+//     if let Some(existing_nav_mesh) = maybe_existing_nav_mesh {
+//         info!("Nav mesh already exists, regenerating!");
+//         generator.regenerate(&existing_nav_mesh.0, nav_mesh_settings);
+//     } else {
+//         let archipelago_options: ArchipelagoOptions<ThreeD> =
+//             ArchipelagoOptions::from_agent_radius(ENEMY_AGENT_RADIUS);
+//
+//         let archipelago_id =
+//             commands.spawn(Archipelago3d::new(archipelago_options)).id();
+//
+//         commands.insert_resource(ArchipelagoRef(archipelago_id));
+//
+//         let navmesh = generator.generate(nav_mesh_settings);
+//
+//         commands.spawn(Island3dBundle {
+//             island: Island,
+//             archipelago_ref: ArchipelagoRef3d::new(archipelago_id),
+//             nav_mesh: NavMeshHandle3d(navmesh),
+//         });
 //     }
 // }
+//
+// fn on_navmesh_ready(
+//     trigger: On<NavmeshReady>,
+//     mut commands: Commands,
+//     mut next_server_loading_state: ResMut<NextState<ServerLoadingState>>,
+//     mut nav_meshes: ResMut<Assets<Navmesh>>,
+// ) {
+//     let Some(nav_mesh_handle) = nav_meshes.get_strong_handle(trigger.0) else {
+//         panic!(
+//             "Got navmeshready event but the Handle could not be found using \
+//              the asset id from the trigger"
+//         );
+//     };
+//
+//     info!("NavMesh is now ready, updating ServerLoadingState to Done");
+//     next_server_loading_state.set(ServerLoadingState::Done);
+//
+//     commands.insert_resource(NavMeshHandle(nav_mesh_handle));
+// }
+//
+// // fn log_agent_state(agent_state: Query<&AgentState>) {
+// //     for agent_state in agent_state {
+// //         info!("Agent state: {:?}", agent_state);
+// //     }
+// // }
